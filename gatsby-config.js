cb6367baa1d09325e634780e093452cb511c5b06
Boris Moreno <boris_marco_moreno@hotmail.com>
2021-05-26 08:18:15 -0500

    Generar robottxt

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  siteMetadata: {
+    title: 'Factura Agil',
+    description: 'Sitio web emision comprobantes',
+    siteUrl: 'https://www.factura-agil.com'
+  },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
@@ -6,6 +11,70 @@ module.exports = {
       options: {
         tailwindConfig: `tailwind.config.js`
       }
-    }
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://www.factura-agil.com',
+        sitemap: 'https://www.factura-agil.com/sitemap.xml',
+        resolveEnv: () => process.env.GATSBY_ENV,
+        env: {
+          development: {
+            policy: [{ userAgent: '*', disallow: ['/'] }]
+          },
+          production: {
+            policy: [{ userAgent: '*', allow: '/' }]
+          }
+        }
+      }
+    },
+    {
+      resolve: "gatsby-plugin-sitemap",
+      options: {
+        query: `
+        {
+          allSitePage {
+            nodes {
+              path
+            }
+          }
+          allWpContentNode(filter: {nodeType: {in: ["Post", "Page"]}}) {
+            nodes {
+              ... on WpPost {
+                uri
+                modifiedGmt
+              }
+              ... on WpPage {
+                uri
+                modifiedGmt
+              }
+            }
+          }
+        }
+      `,
+        resolveSiteUrl: () => siteUrl,
+        resolvePages: ({
+          allSitePage: { nodes: allPages },
+          allWpContentNode: { nodes: allWpNodes },
+        }) => {
+          const wpNodeMap = allWpNodes.reduce((acc, node) => {
+            const { uri } = node
+            acc[uri] = node
+
+            return acc
+          }, {})
+
+          return allPages.map(page => {
+            return { ...page, ...wpNodeMap[page.path] }
+          })
+        },
+        serialize: ({ path, modifiedGmt }) => {
+          return {
+            url: path,
+            lastmod: modifiedGmt,
+          }
+        },
+      },
+    },
   ]
 };

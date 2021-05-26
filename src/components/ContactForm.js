cdb564e93cd1485661dec9a93b355905535aac86
Boris Moreno <boris_marco_moreno@hotmail.com>
2021-05-26 16:06:15 -0500

    Registro contacto

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,74 +4,57 @@ const ContactForm = () => {
     return ( 
         <div className='container mx-auto my-20 w-full xl:w-1/3 md:w-1/2 border border-primary bg-white'>
             <div className='p-5 space-y-5 shadow-xl'>
-                <form>
+                <form method='post' action='https://api.factura-agil.com/api/empresa/contacto'>
                     <div className='grid grid-cols-2 gap-5'>
                         <input 
-                            className='border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500'
+                            className='border border-gray-500 px-4 py-2 focus:outline-none focus:border-primary-lighter'
                             type='text'
+                            name='nombre'
+                            id='nombre'
                             placeholder='Nombre'
                             autoComplete='off'
                         />
                         <input 
-                            className='border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500'
+                            className='border border-gray-500 px-4 py-2 focus:outline-none focus:border-primary-lighter'
                             type='text'
+                            name='apellido'
+                            id='apellido'
                             placeholder='Apellido'
                             autoComplete='off'
                         />
                         <input 
-                            className='border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500 col-span-2'
+                            className='border border-gray-500 px-4 py-2 focus:outline-none focus:border-primary-lighter col-span-2'
                             type='text'
+                            name='email'
+                            id='email'
                             placeholder='Email'
                             autoComplete='off'
                         />
                         <input 
-                            className='border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500 col-span-2'
+                            className='border border-gray-500 px-4 py-2 focus:outline-none focus:border-primary-lighter col-span-2'
                             type='text'
+                            name='telefono'
+                            id='telefono'
                             placeholder='Teléfono'
                             autoComplete='off'
                         />
                         <textarea
                             cols='10'
                             rows='5'
-                            placeholder='Escribe tu mensaje'
-                            className='border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500 col-span-2 resize-none'
+                            name='comentario'
+                            id='comentario'
+                            placeholder='Escribe tu comentario'
+                            className='border border-gray-500 px-4 py-2 focus:outline-none focus:border-primary-lighter col-span-2 resize-none'
                             autoComplete='off'
                         ></textarea>
                     </div>
-                    <input 
+                    <button
                         type='submit'
                         className='focus:outline-none mt-5 bg-primary px-4 py-2 text-white font-bold w-full cursor-pointer hover:bg-primary-darker'
-                        value='Enviar Mensaje' 
-                    />
+                    >Registrarse</button>
                 </form>
 
             </div>
-            {/* <form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4' method="post" action="#">
-                <div className='mb-4'>
-                    <label className='block text-gray-700 text-sm font-bold mb-2'>
-                        Nombre
-                        <input
-                            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                            type="text" name="name" id="name" 
-                        />
-                    </label>
-                </div>
-                <label>
-                    Email
-                    <input type="email" name="email" id="email" />
-                </label>
-                <label>
-                    Subject
-                    <input type="text" name="subject" id="subject" />
-                </label>
-                <label>
-                    Message
-                    <textarea name="message" id="message" rows="5" />
-                </label>
-                <button type="submit">Send</button>
-                <input type="reset" value="Clear" />
-            </form> */}
-
         </div>
      );
 }
